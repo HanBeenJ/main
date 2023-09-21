2bacaab069074540043f4b78ff98c6c176bdf645
MediaLab <MediaLab@DESKTOP-DU01E7K>
2023-09-21 14:51:35 +0900

    19105161 정한빈 23/9/21 수업시간

---
+++ Hello/src/WhileSampleDoWhile.java
@@ -0,0 +1,18 @@
+
+public class WhileSampleDoWhile {
+
+	public static void main(String args[]) {
+		char c = 'a';
+		
+		do{
+			System.out.print(c);
+			c=(char)(c+1);
+		}
+		
+		while (c<= 'z');
+		// TODO Auto-generated method stub
+
+	}
+}
+
+
